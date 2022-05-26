b3faa0178bd862a9dc47df23cfa01e74822cfbce
Tomas Votruba <tomas.vot@gmail.com>
2022-05-27 01:43:33 +0200

    fixup! apply on tests

--- tests/PropertyTypeReader/ScalarPropertyTypeTest.php
+++ tests/PropertyTypeReader/ScalarPropertyTypeTest.php
@@ -4,8 +4,6 @@ declare(strict_types = 1);
 
 namespace ScrumWorks\PropertyReader\Tests\PropertyTypeReader;
 
-use ScrumWorks\PropertyReader\VariableType\ScalarVariableType;
-
 class ScalarPropertyTypeTestClass
 {
     /** @var int */
