5341ebe7a533f1083e13c912cf623d01e2d8b19e
David Dvorak <dvorak.dave@gmail.com>
2023-03-10 08:10:26 +0100

    [phpunit] Fix test

--- tests/VariableType/UnionVariableTypeTest.php
+++ tests/VariableType/UnionVariableTypeTest.php
@@ -25,7 +25,7 @@ class UnionVariableTypeTest extends TestCase
     public function testInputTypesMustBeVariableTypeInterface(): void
     {
         $this->expectException(InvalidArgumentException::class);
-        $this->expectErrorMessage("Given type 'integer' doesn't implements ScrumWorks\PropertyReader\VariableType\VariableTypeInterface");
+        $this->expectErrorMessage("Given type 'int' doesn't implements ScrumWorks\PropertyReader\VariableType\VariableTypeInterface");
         // @phpstan-ignore-next-line
         new UnionVariableType([1, new MixedVariableType()], false);
     }
